7481b5a853b67010b5b1accf1b041f557757c271
Hoa0304 <chtran030404@gmail.com>
2024-07-10 22:00:35 +0700

    arrow function

---
+++ mjs/function/arrow.js
@@ -0,0 +1,10 @@
+let f01 = function (i) {
+    return i * i;
+};
+let f02 = function (i) {
+    return i * i;
+};
+let f03 = (i) => {
+    return i * i;
+};
+let f04 = (i) => i * i;
---
+++ ts/function/arrow.ts
@@ -0,0 +1,13 @@
+let f01 = function(i: number): number {
+    return i*i;
+}
+
+let f02 = function(i: number) {
+    return i*i;
+}
+
+let f03 = (i: number) : number => {
+    return i*i;
+}
+
+let f04 = (i: number)  => i*i;
